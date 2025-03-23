60d4dd2a4a23a047a36ee8c24da76068cd78e4ce
Ringo727 <shibiparfait@gmail.com>
2025-03-23 05:48:57 -0500

    Added basic reaction functionality and basic bot reaction awareness

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 from typing import Final
 import os
 from dotenv import load_dotenv
-from discord import Intents, Client, Message
-from responses import get_response
+from discord import Intents, Client, Message, Reaction, User, Member
+from typing import Union
+import responses
 
 
 # Loading Token
@@ -15,6 +16,9 @@ TOKEN: Final[str] = token_env
 # Bot Setup
 intents: Intents = Intents.default()
 intents.message_content = True
+intents.reactions = True
+intents.members = True
+intents.guilds = True
 client: Client = Client(intents=intents)
 
 # Message Functionality
@@ -27,10 +31,47 @@ async def send_message(message: Message, user_message: str) -> None: # should re
         user_message = user_message[1:]
 
     try:
-        response: str = get_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
+        response: str = responses.get_response(user_message)
+        bot_message: Message = await message.author.send(response) if is_private else await message.channel.send(response)
+        
+        if response == "Here's your card!":
+            await bot_message.add_reaction("✅")
+            await bot_message.add_reaction("🤷")
+            await bot_message.add_reaction("❌")
+            
+            # this is after the message gets updated with the reactions
+            # Note: message.channel is where the message was sent (doesn't matter DM or server text channel)
+            
+            # updated_bot_message = await message.channel.fetch_message(bot_message.id)
+
+            # reaction_list: list[Reaction] = updated_bot_message.reactions
+
+            # if reaction_list[0].count > 1:
+            #     # react_response: Message = await message.channel.send(f"You reacted to the {reaction_list[0].emoji}")
+            #     await message.channel.send(f"You reacted to the {reaction_list[0].emoji}")
+            # elif reaction_list[1].count > 1:
+            #     await message.channel.send(f"You reacted to the {reaction_list[1].emoji}")
+            # elif reaction_list[2].count > 1:
+            #     await message.channel.send(f"You reacted to the {reaction_list[2].emoji}")
+
+
     except Exception as e:
         print(e)
+
+# Reaction Handling
+@client.event
+async def on_reaction_add(reaction: Reaction, user: Union[User, Member]):
+    # Note: in guild contexts, user can be a Member and not just a User
+    emoji = reaction.emoji
+    if user.bot:
+        return
+
+    if emoji == "✅":
+        await reaction.message.channel.send(f"{user.name} reacted with a {reaction} on message with id: {reaction.message.id}")
+    elif emoji == "🤷":
+        await reaction.message.channel.send(f"{user.name} reacted with a {reaction} on message with id: {reaction.message.id}")
+    elif emoji == "❌":
+        await reaction.message.channel.send(f"{user.name} reacted with a {reaction} on message with id: {reaction.message.id}")
 
 # Startup Handling
 @client.event
--- responses.py
+++ responses.py
@@ -9,7 +9,13 @@ def get_response(user_input: str) -> str:
         return "Hello there!"
     elif "roll dice" in lowered:
         return f"You Rolled: {randint(1, 6)}"
-    else:
-        return choice(["I don't understand...", 
-                        "What are you saying...?",
-                        "Try that again..."])
+    # else:
+        # return choice(["I don't understand...", 
+        #                 "What are you saying...?",
+        #                 "Try that again..."])
+    
+    if "give card" in lowered:
+        return "Here's your card!"
+    
+# def reaction_response() -> str:
+    # return ""
